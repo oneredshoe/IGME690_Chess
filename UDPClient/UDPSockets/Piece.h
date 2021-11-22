f96878c33611750bf25c711785246f7bae1f16dd
Wren Bernstein <wrenbernstein@gmail.com>
2021-11-22 14:54:52 -0500

    Still Linker errors, however the pieces now draw themselves!
    Changes:
    Pieces now need a render window in their constructors (both of them, there is no default anymore)
    Pieces all connect properly to their inherited piece class (I hope)

--- UDPClient/UDPSockets/Piece.h
+++ UDPClient/UDPSockets/Piece.h
@@ -1,5 +1,12 @@
+#pragma once
+#define SFML_Static
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+
 #ifndef PIECE_H
 #define PIECE_H
+
+
 #include <string>
 #include <vector>
 #include <array>
@@ -32,8 +39,8 @@ enum Letters {A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7};
 class Piece
 {
 public:
-	Piece(PieceType name, int x, int y, bool isBlack);
-	Piece();
+	Piece(PieceType name, int x, int y, bool isBlack, sf::RenderWindow& win);
+	Piece(sf::RenderWindow& win);
 	~Piece();
 	PieceType getName();
 	int* getPosition();
@@ -50,43 +57,50 @@ protected:
 	PieceType m_name;
 	int m_position[2]{0, 0};
 	bool m_isBlack;
+	sf::RenderWindow& m_window;
 	vector<array<int, 2>> CheckSides( int board[8][8]);
 	vector<array<int, 2>> CheckDiagonals(int board[8][8]);
 };
 
 class Horse : public Piece {
 public:
-	Horse(int x, int y, bool isBlack);
+	Horse(int x, int y, bool isBlack, sf::RenderWindow& win);
 	vector<array<int, 2>>PossibleMoves(int board[8][8]) override;
+	void Draw();
 };
 class King : public Piece {
 public:
-	King(int x, int y, bool isBlack);
+	King(int x, int y, bool isBlack, sf::RenderWindow& win);
 	vector<array<int, 2>>PossibleMoves(int board[8][8]) override;
+	void Draw();
 };
 
 class Queen : public Piece {
 public:
-	Queen(int x, int y, bool isBlack);
+	Queen(int x, int y, bool isBlack, sf::RenderWindow& win);
 	vector<array<int, 2>>PossibleMoves(int board[8][8]) override;
+	void Draw();
 };
 class Bishop : public Piece {
 public:
-	Bishop(int x, int y, bool isBlack);
+	Bishop(int x, int y, bool isBlack, sf::RenderWindow& win);
 	vector<array<int, 2>>PossibleMoves(int board[8][8]) override;
+	void Draw();
 };
 
 class Castle : public Piece {
 public:
-	Castle(int x, int y, bool isBlack);
+	Castle(int x, int y, bool isBlack, sf::RenderWindow& win);
 	vector<array<int, 2>>PossibleMoves(int board[8][8]) override;
+	void Draw();
 };
 class Pawn : public Piece
 {
 public:
-	Pawn(int x, int y, bool isBlack);
+	Pawn(int x, int y, bool isBlack, sf::RenderWindow& win);
 	bool Move(int position[], int board[8][8]) override;
 	vector<array<int, 2>>PossibleMoves(int board[8][8]) override;
+	void Draw();
 
 private:
 	bool m_hasMoved;
